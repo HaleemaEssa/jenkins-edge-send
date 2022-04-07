603a1126283593c65769b6b9aefadc5ed37346e7
HaleemaEssa <70490977+HaleemaEssa@users.noreply.github.com>
2022-04-08 01:16:20 +0300

    Create pp2.py

---
+++ pp2.py
@@ -0,0 +1,49 @@
+import pika, sys, os
+import datetime
+from datetime import datetime
+import time
+import csv
+#import pandas
+import pika, os, logging
+logging.basicConfig()
+from csv import reader
+#import pandas as pd
+
+url = os.environ.get('CLOUDAMQP_URL', 'amqps://kacojdss:aUd8wEoKcyHLCK46a1_AifxUBDzjsLHi@beaver.rmq.cloudamqp.com/kacojdss')
+params = pika.URLParameters(url)
+params.socket_timeout = 5
+connection = pika.BlockingConnection(params) # Connect to CloudAMQP
+channel = connection.channel() # start a channel
+channel.queue_declare(queue='pdfprocess') # Declare a queue
+# send a message
+##import pandas as pd
+##df=pd.read_csv('data1.csv')
+##for row in df:
+#with open('data1.csv', 'r') as read_obj:
+ #   csv_reader = reader(read_obj)
+   # header = next(csv_reader)
+
+csv.register_dialect('csv_dialect',
+                    delimiter='[',
+                    skipinitialspace=True,
+                    quoting=csv.QUOTE_ALL)
+with open('/data/data1.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, dialect='csv_dialect')
+    header=next(reader)
+   
+ # Check file as empty
+    if header != None:
+#import pandas as pd
+#df=pd.read_csv('data1.csv')
+        for row in reader:
+   # row variable is a list that represents a row in csv
+#import pandas as pd
+#df=pd.read_csv('data1.csv') #,usecols=['Date', 'Sound', 'Flame', 'Humidity', 'Temperature']) 
+
+#for row in 'data1.csv': 
+          print(row)
+          channel.basic_publish(exchange='', routing_key='pdfprocess', body=str(row))
+print ("[x] Message sent to consumer")
+connection.close()
+        
+        
